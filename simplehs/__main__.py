a425ad1d1725382f8f40b558aee72fc1e9ebabaa
Wentao Han <wentao.han@gmail.com>
2014-06-22 02:50:18 +0800

    Add program arguments.

--- simplehs/__main__.py
+++ simplehs/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Simple Hearthstone
 
+import argparse
 import logging
 import sys
 
@@ -14,7 +15,30 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def main(args=sys.argv[1:]):
-    seed = 1 if not args else args[0]
+    parser = argparse.ArgumentParser(
+        description='Simulate a Hearthstone match.')
+    parser.add_argument('-s', '--seed', type=int, default=1,
+                        help='random seed')
+    parser.add_argument('--client1', default='NaiveClient',
+                        help='client class of player 1')
+    parser.add_argument('--client2', default='NaiveClient',
+                        help='client class of player 2')
+    parser.add_argument('--name1', default='Alice',
+                        help='name of player 1')
+    parser.add_argument('--name2', default='Bob',
+                        help='name of player 2')
+    parser.add_argument('--hero1', default='InnKeeper',
+                        help='hero class of player 1')
+    parser.add_argument('--hero2', default='InnKeeper',
+                        help='hero class of player 2')
+    args = parser.parse_args()
+    seed = args.seed
+    client1_class = globals()[args.client1]
+    client2_class = globals()[args.client2]
+    name1 = args.name1
+    name2 = args.name2
+    hero1_class = globals()[args.hero1]
+    hero2_class = globals()[args.hero2]
     deck = Deck(
         [MurlocRaider()] * 9 +
         [BloodfenRaptor()] * 5 +
@@ -25,8 +49,8 @@ def main(args=sys.argv[1:]):
         [CoreHound()] * 1 +
         [WarGolem()] * 1 +
         [])
-    client1 = NaiveClient('Alice', InnKeeper, deck)
-    client2 = NaiveClient('Bob', InnKeeper, deck)
+    client1 = client1_class(name1, hero1_class, deck)
+    client2 = client2_class(name2, hero2_class, deck)
     match = Match(client1, client2, seed)
     match.run()
 
